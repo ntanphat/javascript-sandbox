f9426ef6e8a74aaaf3dd950466610512d1070e04
Phat Nguyen <nguyentanphat1304@gmail.com>
2025-10-20 22:20:47 +0700

    init

---
+++ 01-variables-data-types/01-consolse/script.js
@@ -0,0 +1,24 @@
+console.log("Hello, World!");
+
+console.log(100)
+
+console.log(10, 'hello', true)
+
+const x = 10;
+
+console.log(x);
+
+console.error('Alert');
+
+console.warn('Warning');
+
+console.table({name: 'John', age: 30});
+
+console.group('My Group');
+console.log(x);
+console.error('Alert');
+console.warn('Warning');
+console.groupEnd('My Group');
+
+const styles = 'padding; 10px; background-color: white; color: red;'
+console.log('%c This is a styled message', styles);
